7918e78ac3e7403ff781642654c624ff6908d3c8
MatsveiDubaleka <matvey.duboleko@yandex.ru>
2025-12-04 01:16:15 +0300

    feat: add header

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,13 +1,57 @@
 import SignInWithGithub from "./SignInWithGithub";
 import SignInWithGoogle from "./SignInWithGoogle";
 
-export default function Header() {
+import { auth } from "@/auth";
+import Image from "next/image";
+
+import { signOut } from "@/auth";
+import { LogOutIcon } from "lucide-react";
+import { Button } from "../ui/button";
+
+export function SignOut() {
+  return (
+    <form
+      action={async () => {
+        "use server";
+        await signOut();
+      }}
+    >
+      <Button type="submit" variant="destructive">
+        <LogOutIcon /> Sign Out
+      </Button>
+    </form>
+  );
+}
+
+export default async function Header() {
+  const session = await auth();
+
+  console.log(session);
+
   return (
     <header className="bg-red-300 mx-auto px-4 py-4">
-      <div className="flex items-center justify-between">
-        <SignInWithGithub />
-        <SignInWithGoogle />
-      </div>
+      {session?.user ? (
+        <div className="flex items-center justify-end gap-4">
+          <div className="flex justify-center items-center gap-2">
+            <p className="text-sm font-medium">{session.user.name}</p>
+            {session.user.image && (
+              <Image
+                src={session.user.image}
+                alt="User Avatar"
+                width={32}
+                height={32}
+                className="rounded-full"
+              />
+            )}
+          </div>
+          <SignOut />
+        </div>
+      ) : (
+        <div className="flex items-end justify-end gap-2">
+          <SignInWithGithub />
+          <SignInWithGoogle />
+        </div>
+      )}
     </header>
   );
 }
